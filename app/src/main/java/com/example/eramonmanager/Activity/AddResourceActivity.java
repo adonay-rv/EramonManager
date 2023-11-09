46175403cc7a3bdc251c11cfda53667c646607a4
owen <owen.esperanza@catolica.edu.sv>
2023-11-08 21:22:41 -0600

    Owen: Solo falta el adapter

--- app/src/main/java/com/example/eramonmanager/Activity/AddResourceActivity.java
+++ app/src/main/java/com/example/eramonmanager/Activity/AddResourceActivity.java
@@ -6,11 +6,113 @@ import android.os.Bundle;
 
 import com.example.eramonmanager.R;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class AddResourceActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
+    private FirebaseStorage storage;
+    private String imageUrl;
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private Button imageButton;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri filePath = data.getData();
+
+            ImageView imagen = findViewById(R.id.ImageResource);
+
+            // Escala la imagen para que se ajuste al tamaño del ImageButton
+            Picasso.get().load(filePath).resize(imagen.getWidth(), imagen.getHeight()).centerCrop().into(imagen);
+
+            imageUrl = filePath.toString();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_resource);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        storage = FirebaseStorage.getInstance();
+
+        imageButton = findViewById(R.id.Button_UploadPhotoVoucher);
+
+        Button button = findViewById(R.id.Button_AddResource);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView nombreRecurso1 = findViewById(R.id.Add_Resource_Name);
+                TextView Cantidad1 = findViewById(R.id.Add_Resource_Mount);
+                String nombreRecurso = nombreRecurso1.getText().toString();
+                String Cantidad = Cantidad1.getText().toString();
+                String idRecursos = nombreRecurso1.getText().toString();
+
+                if (!TextUtils.isEmpty(imageUrl)) {
+                    Date date = new Date();
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+                    String fechaActual = dateFormat.format(date);
+
+                    Recursos recursos = new Recursos();
+
+                    recursos.buscarEnFirebase(idRecursos, new Recursos.BusquedaCallback() {
+                        @Override
+                        public void onResultado(boolean encontrado) {
+                            if (encontrado) {
+                                recursos.Actualizar(idRecursos, Integer.parseInt(Cantidad), nombreRecurso, imageUrl, fechaActual);
+                            } else {
+                                recursos.Crear(idRecursos, nombreRecurso, Integer.parseInt(Cantidad), imageUrl, fechaActual);
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toOpenGallery();
+            }
+        });
+
     }
+
+    private void toOpenGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+
 }
---
+++ app/src/main/java/com/example/eramonmanager/Activity/Recursos.java
@@ -0,0 +1,149 @@
+package com.example.eramonmanager.Activity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Recursos {
+    private String idRecursos;
+    private String nombreRecurso;
+    private int cantidadRecurso;
+    private String imagenUrl;
+    private String fechaActualizacion;
+
+    // Constructor vacío requerido para Firebase
+    public Recursos() {
+        // Constructor vacío requerido para Firebase
+    }
+
+    public String getImagenUrl() {
+        return imagenUrl;
+    }
+
+    public void setImagenUrl(String imagenUrl) {
+        this.imagenUrl = imagenUrl;
+    }
+
+    public String getFechaActualizacion() {
+        return fechaActualizacion;
+    }
+
+    public void setFechaActualizacion(String fechaActualizacion) {
+        this.fechaActualizacion = fechaActualizacion;
+    }
+
+
+    public Recursos(String idRecursos, String nombreRecurso, int cantidadRecurso, String imagenUrl, String fechaActualizacion) {
+        this.idRecursos = idRecursos;
+        this.nombreRecurso = nombreRecurso;
+        this.cantidadRecurso = cantidadRecurso;
+        this.imagenUrl = imagenUrl;
+        this.fechaActualizacion = fechaActualizacion;
+    }
+
+
+    public String getIdRecursos() {
+        return idRecursos;
+    }
+
+
+    public void setIdRecursos(String idRecursos) {
+        this.idRecursos = idRecursos;
+    }
+
+
+    public String getNombreRecurso() {
+        return nombreRecurso;
+    }
+
+
+    public void setNombreRecurso(String nombreRecurso) {
+        this.nombreRecurso = nombreRecurso;
+    }
+
+
+    public int  getCantidadRecurso() {
+        return cantidadRecurso;
+    }
+
+
+    public void setCantidadRecurso(int  cantidadRecurso) {
+        this.cantidadRecurso = cantidadRecurso;
+    }
+
+
+
+    public interface BusquedaCallback {
+        void onResultado(boolean encontrado);
+    }
+
+    public void buscarEnFirebase(String dato, BusquedaCallback callback) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference recursosRef = database.getReference("CRUD Recursos ERAMON");
+
+        recursosRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                boolean encontrado = false;
+
+                for (DataSnapshot nodeSnapshot : dataSnapshot.getChildren()) {
+                    String idRecursos = nodeSnapshot.child("idRecursos").getValue(String.class);
+                    if (idRecursos != null && idRecursos.equals(dato)) {
+                        encontrado = true;
+                        break;
+                    }
+                }
+
+                // Llama al método de devolución de llamada con el resultado
+                callback.onResultado(encontrado);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Manejar errores
+            }
+        });
+    }
+
+
+
+    public void Actualizar(String idRecurso, int nuevaCantidad, String nuevoNombre, String nuevaImagenUrl, String nuevaFecha) {
+        DatabaseReference recursosRef = FirebaseDatabase.getInstance().getReference("CRUD Recursos ERAMON");
+        String idRecursosAActualizar = idRecurso;
+
+        Map<String, Object> actualizacion = new HashMap<>();
+        actualizacion.put("nombreRecurso", nuevoNombre);
+        actualizacion.put("cantidadRecurso", nuevaCantidad);
+        actualizacion.put("imagenUrl", nuevaImagenUrl);
+        actualizacion.put("fechaActualizacion", nuevaFecha);
+
+        recursosRef.child(idRecursosAActualizar).updateChildren(actualizacion);
+    }
+
+    public void Crear(String idRecursos, String nombreRecurso, int cantidad, String imagenUrl, String fecha) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference recursosRef = database.getReference("CRUD Recursos ERAMON");
+
+        Recursos recurso = new Recursos(idRecursos, nombreRecurso, cantidad, imagenUrl, fecha);
+
+        recursosRef.child(idRecursos).setValue(recurso);
+    }
+
+
+    public void Eliminar(String Eliminar){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference recursosRef = database.getReference("CRUD Recursos ERAMON");
+
+        String idRecursosAEliminar = Eliminar;
+        recursosRef.child(idRecursosAEliminar).removeValue();
+
+    }
+
+}
+
+
